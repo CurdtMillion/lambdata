6ff8f8fd6e32bfc1f5d170a43f997d2a778b7764
CurdtMillion <curtcecil@gmail.com>
2020-05-16 17:52:09 -0500

    Added BaseballTeam Class

--- my_lambdata/teams.py
+++ my_lambdata/teams.py
@@ -1,7 +1,7 @@
 # example teams.py (oop approach)
 
 class Team():
-    def __init__(self, name, city='CITY NAME', players=[]):
+    def __init__(self, name, city='CITY NAME', players=['Player 1']):
         self.name = name
         self.city = city
         self.players = players
@@ -16,23 +16,47 @@ class Team():
         #return f"{my_team['city']} {my_team['name']}"
         return f"{self.city} {self.name}"
 
+class BaseballTeam():
+    def __init__(self, name, city, starting_pitcher, players=['Player 1']):
+        self.name = name
+        self.city = city
+        self.players = players
+        self.starting_pitcher = starting_pitcher
+
+
+    def advertise(self):
+        #print(f"HEY COME TO {my_team['city'].upper()} TO SEE OUR GAMES!!!")
+        print(f"HEY COME TO {self.city.upper()} TO SEE OUR GAMES!!!")
+
+    @property
+    def full_name(self):
+        #return f"{my_team['city']} {my_team['name']}"
+        return f"{self.city} {self.name}"
+
 if __name__ == "__main__":
+
+    football_team = Team("Dalls", "Cowboys")
+    print(football_team.full_name)
+    print(football_team.advertise())
+
+
     teams = [
-        {"city": "New York", "name": "Yankees"},
-        {"city": "New York", "name": "Mets"},
-        {"city": "Boston", "name": "Red Sox"},
-       {"city": "New Haven", "name": "Ravens"},
-        {"city": "Washington", "name": "Nationals"}
+        {"city": "New York", "name": "Yankees", "pitcher": "John"},
+        {"city": "New York", "name": "Mets", "pitcher": "Jane"},
+        {"city": "Boston", "name": "Red Sox", "pitcher": "Amy"},
+        {"city": "New Haven", "name": "Ravens", "pitcher": "Henry"},
+        {"city": "Washington", "name": "Nationals", "pitcher": "Ember"}
     ]
 
     for team_attributes in teams:
-        team = Team(name= team_attributes['name'], city=team_attributes['city'])
+        team = BaseballTeam(name=team_attributes['name'], city=team_attributes['city'], starting_pitcher=team_attributes['pitcher'])
         print("-------")
         #print(full_name(team))
         #advertise(team)
         print(team.city)
         print(team.full_name)
         print(team.players)
+        print(team.starting_pitcher)
         team.advertise()
     
     # df1 = DataFrame({'x': [1,2,3], 'y': [4,5,6]})
