11da5c8848f85868173ba04a29dfaac77627401c
CurdtMillion <curtcecil@gmail.com>
2020-05-16 17:31:12 -0500

    oop approach finished

--- my_lambdata/teams.py
+++ my_lambdata/teams.py
@@ -1,4 +1,4 @@
-# example teams.py (working on oop approach)
+# example teams.py (oop approach)
 
 class Team():
     def __init__(self, name, city):
@@ -6,25 +6,32 @@ class Team():
         self.city = city
 
 
-def advertise(my_team):
-    print(f"HEY COME TO {my_team['city'].upper()} TO SEE OUR GAMES!!!")
+    def advertise(self):
+        #print(f"HEY COME TO {my_team['city'].upper()} TO SEE OUR GAMES!!!")
+        print(f"HEY COME TO {self.city.upper()} TO SEE OUR GAMES!!!")
 
-def full_name(my_team):
-    return f"{my_team['city']} {my_team['name']}"
+    @property
+    def full_name(self):
+        #return f"{my_team['city']} {my_team['name']}"
+        return f"{self.city} {self.name}"
 
 if __name__ == "__main__":
-    #teams = [
-    #    {"city": "New York", "name": "Yankees"},
-    #    {"city": "New York", "name": "Mets"},
-    #    {"city": "Boston", "name": "Red Sox"},
-    #   {"city": "New Haven", "name": "Ravens"},
-    #    {"city": "Washington", "name": "Nationals"}
-    #]
+    teams = [
+        {"city": "New York", "name": "Yankees"},
+        {"city": "New York", "name": "Mets"},
+        {"city": "Boston", "name": "Red Sox"},
+       {"city": "New Haven", "name": "Ravens"},
+        {"city": "Washington", "name": "Nationals"}
+    ]
 
-    #for team in teams:
-    #    print("-------")
-    #   print(full_name(team))
-    #    advertise(team)
+    for team_attributes in teams:
+        team = Team(name= team_attributes['name'], city=team_attributes['city'])
+        print("-------")
+        #print(full_name(team))
+        #advertise(team)
+        print(team.city)
+        print(team.full_name)
+        team.advertise()
     
     # df1 = DataFrame({'x': [1,2,3], 'y': [4,5,6]})
     # df1.head()
@@ -32,14 +39,14 @@ if __name__ == "__main__":
     # df2 = DataFrame({'x': [7,7,7], 'y': [4,4,4]})
     # df2.head()
 
-    team = Team(city='Washington', name='Wizards')  # initializ/create instance of objec
-    print(team)
-    print(type(team))
-    print(team.name) # invoking attributes
-    print(team.city) # invoking attributes
+    #team = Team(city='Washington', name='Wizards')  # initializ/create instance of objec
+    #print(team)
+    #print(type(team))
+    #print(team.name) # invoking attributes
+    #print(team.city) # invoking attributes
 
-    team2 = Team('Giants', 'New York')  # initializ/create instance of objec
-    print(team2)
-    print(type(team2))
-    print(team2.name) # invoking attributes
-    print(team2.city) # invoking attributes
+    #team2 = Team('Giants', 'New York')  # initializ/create instance of objec
+    #print(team2)
+    #print(type(team2))
+    #print(team2.name) # invoking attributes
+    #print(team2.city) # invoking attributes
