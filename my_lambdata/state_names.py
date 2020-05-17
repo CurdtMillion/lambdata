5eb691f32b64711bc407844030e14985540db899
CurdtMillion <curtcecil@gmail.com>
2020-05-17 18:11:53 -0500

    added YouTube follow along 'YT_OOP'

--- my_lambdata/state_names.py
+++ my_lambdata/state_names.py
@@ -1,31 +1,26 @@
 
 from pandas import DataFrame
 
-def add_state_names(my_df):
+class DataProcessor():
+    def __init__(self, something_else):
+        '''
+        Params: something_else (pandas.DataFrame) has a column cal
+        '''
+        self.df = something_else
+
+    def add_state_names(self):
     '''
     Adds a column of state names to accompany a corresponding column of abbrev
-
-    Params:
-        my_df (pandas.DataFrame) has a column called "abbrev" with state abbrevs
-
-    Returns:
-        copy of the original dataframe, with another column
     '''
-    new_df = my_df.copy()
     names_map = {'CA': 'Cali', 'CO': 'Colo', 'CT': 'Conn'}
-    new_df['name'] = new_df['abbrev'].map(names_map)
-    return new_df
+    self.df['name'] = self.df['abbrev'].map(names_map)
 
 if __name__ == '__main__':
 
     df = DataFrame({'abbrev': ['CA', 'CO', 'CT', 'DC', 'TX']})
 
-    print(df.columns)
-    print(df.head())
+    processor = DataProcessor(df)
+    print(processor.df.head())
 
-    df2 = add_state_names(df)
-    print(df2.head())
-
-    df3 = DataFrame({'a': [1, 2, 3, 4]})
-    print(df3.head())
-
+    processor.add_state_names()
+    print(processor.df.head())
